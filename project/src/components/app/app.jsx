8ba113f4cb4129262c2e13ffb24c318916ce210b
Julia <zakr.jv@gmail.com>
2021-06-07 20:21:26 +0800

    Добавляет компоненты

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MainPage from '../main-page/main-page';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({offersCount}) {
+  return (
+    <MainPage offersCount={offersCount} />
+  );
 }
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+};
 
 export default App;
