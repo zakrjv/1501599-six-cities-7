0c2f31aa8bedc4c163fde7bd566f06a500932fc6
Julia <zakr.jv@gmail.com>
2021-06-29 21:16:06 +0800

    Инициализирует новое хранилище

--- project/src/index.js
+++ project/src/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {offers} from './mocks/offers';
 import {reviews} from './mocks/reviews';
 import {city} from './mocks/city';
+import {reducer} from './store/reducer';
 
 const Settings = {
   OFFERS_COUNT: 312,
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offersCount={Settings.OFFERS_COUNT}
-      offers={offers}
-      reviews={reviews}
-      city={city}
-    />
+    <Provider store={store}>
+      <App
+        offersCount={Settings.OFFERS_COUNT}
+        offers={offers}
+        reviews={reviews}
+        city={city}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
--- project/src/store/reducer.js
+++ project/src/store/reducer.js
@@ -12,7 +12,7 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         currentCity: action.payload,
-      }
+      };
     case ActionType.FILL_OFFERS:
       return {
         ...state,
